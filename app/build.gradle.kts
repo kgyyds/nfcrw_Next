833bcc2148973746f5e72937d5315d87ff7fc6b2
kgyyds <1361906712@qq.com>
2025-12-23 22:37:10 +0800

    未声明的修复

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-        // Enable Java 8+ API desugaring
-        isCoreLibraryDesugaringEnabled = true
+        
     }
     kotlinOptions {
         jvmTarget = "11"
