9c732bf056d6f5abaadb50d2265641cba87d762d
kgyyds <1361906712@qq.com>
2025-12-23 23:04:30 +0800

    未声明的修复

--- app/src/main/java/com/kgapp/nfcrw/MainActivity.kt
+++ app/src/main/java/com/kgapp/nfcrw/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.unit.dp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         setContent {
             MaterialTheme {
